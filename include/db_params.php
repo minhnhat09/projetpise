5ee3a8debd25f723a2f3f2e8d6097b479ee21aee
minhnhat09 <nhatminh1809@gmail.com>
2019-07-04 08:43:26 +0200

    change db param for prod

--- include/db_params.php
+++ include/db_params.php
@@ -1,9 +1,9 @@
 <?php
 $TYPEBD="mysql";
-$SERVEUR="localhost";
-$BASE="ttart";
-$LOGIN="giang";
-$MDP='psm20162017';
+$SERVEUR="us-cdbr-iron-east-02.cleardb.net";
+$BASE="heroku_0e09e525c75a9d4";
+$LOGIN="b398e3ebd37f26";
+$MDP='6f50b283';
 
 
 $OEUVRE="oeuvre";
